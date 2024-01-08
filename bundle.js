fea94eb09502654d6b8cb04a732f77a862007f9f
Sequoia Kanies <sequoia.mathias.kanies@gmail.com>
2024-01-08 17:54:35 -0500

    CSS safe-area and translucent status bar

--- bundle.js
+++ bundle.js
@@ -1,27 +1,3 @@
-// Code from this point until the microsoft rollup software is custom
-let loginForm = document.getElementById('submit-form');
-
-loginForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let name = document.getElementById("name").value;
-    let email = document.getElementById("email").value;
-    let subscribed = document.querySelector('#subscribe-checkbox').checked;
-    let selectHealthOptions = document.getElementById('select-health-options').value;
-    let allergySwitch = document.getElementById('allergy-switch').selected;
-    
-    console.log('name: = ', name);
-    console.log('email: = ', email);
-    console.log('subscribed: = ', subscribed);
-    console.log('selectHealthOptions: = ', selectHealthOptions);
-    console.log('allergySwitch: = ', allergySwitch);
-  });
-
-  const anchorEl = document.body.querySelector('#usage-popover-anchor');
-  const menuEl = document.body.querySelector('#usage-popover');
-
-  anchorEl.addEventListener('click', () => { menuEl.open = !menuEl.open; });
-
-
 /******************************************************************************
 Copyright (c) Microsoft Corporation.
 
@@ -7495,3 +7471,34 @@ MdElevatedButton.styles = [styles$m, styles$n, styles];
 MdElevatedButton = __decorate([
     t$3('md-elevated-button')
 ], MdElevatedButton);
+
+// import '@material/web/popover/popover.js';
+
+
+
+// code in this file can be added to the bundle.js file after the command 
+// rollup, to add this functionality. This is merely a placeholder file, because 
+// this code gets deleted from the bundle.js file after the command rollup.
+
+let loginForm = document.getElementById('submit-form');
+loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let subscribed = document.querySelector('#subscribe-checkbox').checked;
+    let selectHealthOptions = document.getElementById('select-health-options').value;
+    let allergySwitch = document.getElementById('allergy-switch').selected;
+    
+    console.log('name: = ', name);
+    console.log('email: = ', email);
+    console.log('subscribed: = ', subscribed);
+    console.log('selectHealthOptions: = ', selectHealthOptions);
+    console.log('allergySwitch: = ', allergySwitch);
+  });
+
+  const anchorEl = document.body.querySelector('#usage-popover-anchor');
+  const menuEl = document.body.querySelector('#usage-popover');
+
+  anchorEl.addEventListener('click', () => { menuEl.open = !menuEl.open; });
+// after adding an import DON'T FORGET TO RUN THE ROLLUP SCRIPT
--- index.js
+++ index.js
@@ -10,5 +10,35 @@ import '@material/web/select/select-option.js';
 import '@material/web/icon/icon.js';
 import '@material/web/iconbutton/icon-button.js';
 import '@material/web/button/elevated-button.js';
+// added these
+import '@material/web/menu/menu.js';
+// import '@material/web/popover/popover.js';
 
+
+
+// code in this file can be added to the bundle.js file after the command 
+// rollup, to add this functionality. This is merely a placeholder file, because 
+// this code gets deleted from the bundle.js file after the command rollup.
+
+let loginForm = document.getElementById('submit-form');
+loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let subscribed = document.querySelector('#subscribe-checkbox').checked;
+    let selectHealthOptions = document.getElementById('select-health-options').value;
+    let allergySwitch = document.getElementById('allergy-switch').selected;
+    
+    console.log('name: = ', name);
+    console.log('email: = ', email);
+    console.log('subscribed: = ', subscribed);
+    console.log('selectHealthOptions: = ', selectHealthOptions);
+    console.log('allergySwitch: = ', allergySwitch);
+  });
+
+  const anchorEl = document.body.querySelector('#usage-popover-anchor');
+  const menuEl = document.body.querySelector('#usage-popover');
+
+  anchorEl.addEventListener('click', () => { menuEl.open = !menuEl.open; });
 // after adding an import DON'T FORGET TO RUN THE ROLLUP SCRIPT
